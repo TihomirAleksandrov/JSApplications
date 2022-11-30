2a7134dbd3b21936b6acac52d4cb4d3e33ca31af
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-11-30 17:12:23 +0200

    Added new files

---
+++ PetCare/src/views/nav.js
@@ -0,0 +1,30 @@
+import { html, render, page } from "../lib.js";
+import { getUserData } from "../util.js";
+import { logout } from '../api/user.js';
+
+const nav = document.querySelector('header');
+const navTemplate = (hasUser) => html`
+<nav>
+            <section class="logo">
+                <img src="./images/logo.png" alt="logo">
+            </section>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/catalog">Dashboard</a></li>
+                ${!hasUser ? html`<li><a class="guest" href="/login">Login</a></li>
+                <li><a class="guest" href="/register">Register</a></li>` : html`<li><a class="user" href="/create">Create Postcard</a></li>
+                <li><a @click=${onLogout} class="user" href="javascript:void(0)">Logout</a></li>`}
+            </ul>
+        </nav>`;
+
+export function updateNav(){
+    const user = getUserData();
+
+    render(navTemplate(user), nav);
+}
+
+function onLogout(){
+    logout();
+    updateNav();
+    page.redirect('/');
+}
---
+++ Skeleton/src/app.js
@@ -0,0 +1,11 @@
+import {page, render} from './lib.js';
+
+page('/', () => console.log('home'));
+page('/catalog', () => console.log('catalog'));
+page('/catalog/:id', () => console.log('details'));
+page('/edit/:id', () => console.log('edit'));
+page('/login', () => console.log('login'));
+page('/register', () => console.log('register'));
+page('/create', () => console.log('create'));
+
+page.start();
